88db1fe01bf4ba79989ffdc41c63becff5d873c8
Haritha <harithsenevi4@gmail.com>
2018-04-16 12:04:09 +0530

    commited registration

---
+++ 1.php
@@ -0,0 +1,22 @@
+<html>
+
+
+<head>
+<body>
+
+<?php 
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$c_password = $_POST["c_password"];
+$university = $_POST["university"];
+$course = $_POST["course"];
+$aboutme = $_POST["aboutme"];
+ ?>
+
+</body>
+
+</head>
+</html>
